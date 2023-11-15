0f71928ca61b0422ca85aa0d356d618d19ec2f55
light-planck <seseifunc16@gmail.com>
2023-11-15 09:31:38 +0900

    feat: bookにauthorを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_033129) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_002616) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
